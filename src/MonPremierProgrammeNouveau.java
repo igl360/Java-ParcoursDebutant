19cc3cd6f7fa0ff39177b1430d3b359674226f9a
otisDeCode <igl.learning360@gmail.com>
2023-05-28 18:17:20 +0200

    module02-Les-commentaires-en-Java

--- src/MonPremierProgrammeNouveau.java
+++ src/MonPremierProgrammeNouveau.java
@@ -1,6 +1,13 @@
 public class MonPremierProgrammeNouveau {
     public static void main(String[] args) {
 
+
+        /*Dans cette fonction ci-dessous nous allons afficher dans la console "Hello World";*/
+
+        //Dans cette fonction ci-dessous nous allons afficher dans la console "Hello World";
+
         System.out.println("Hello world!");
+
+
     }
 }
