42f59f76afa598baa677480f4c9f0c84e3f1cb4c
otisDeCode <igl.learning360@gmail.com>
2023-08-01 23:12:30 +0200

    first commit

--- src/TypesDeDonnes.java
+++ src/TypesDeDonnes.java
@@ -1,52 +1,110 @@
+import java.util.Arrays;
+
 public class TypesDeDonnes {
     public static void main(String[] args) {
+        // Retirer les commentaires pour accéder au code
+
+/*        String prenom = "Jean Philippe";
+
+        System.out.println("Mon prénom est: " + prenom);
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        String prenom = new String("Jean Philippe");
+
+        System.out.println("Mon prénom est: " + prenom);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        String prenom = "Je me nomme \"Jean Philippe\"";
+
+        System.out.println(prenom);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        String prenom = "Je me nomme \n Jean Philippe";
+
+        System.out.println(prenom);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // Retirer les commentaires pour accéder au code
 
+        String prenom = "Je me nomme \t Jean Philippe";
 
-/*        byte variableByte= 70;
-        System.out.println("La valeur de variableByte est " + variableByte);
-
-        byte negatifByte = -90;
-        System.out.println("La valeur de negatifByte est " + negatifByte);
-
-        //Ensuite j'augmente la valeur de la variable pour montrer la limite
-        byte newVariableByte = 127;
-        System.out.println("La valeur de newVariableByte est " + newVariableByte);
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        short variableShort = 32767;
-        System.out.println("La valeur de variableShort est " + variableShort);
-
-        short negatifShort = -32767;
-        System.out.println("La valeur de negatifShort est " + negatifShort);
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        char lettre = "Z";
-        System.out.println("La valeur de lettre est " + lettre);
-
-        char variableUnicode = '\u0052';
-        System.out.println("La valeur de variableUnicode est " + variableUnicode);
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        boolean laTerreEstRonde = true;
-        System.out.println("La terre est ronde? : " + laTerreEstRonde);
-
-        boolean leCielBleu = false;
-        System.out.println("Le ciel est toujours bleu? : " + leCielBleu);
+        System.out.println(prenom);
 
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        int nombre1 = 35;
-        int nombre2 = 59;
+        String prenom = "Je me nomme\b Jean Philippe";
 
-        boolean resultat = nombre1 > nombre2;
-        System.out.println("Le nombre 1 est-il supérieur au nombre 2? : " + resultat);*/
+        System.out.println(prenom);
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        String [] collectionDeVoitures = {"Hummer", "Aston Martin", "Mercedes", "Toyota"};
+
+        System.out.println("Ma collection de voiture: " + collectionDeVoitures);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        import java.util.Arrays;
+
+        String [] collectionDeVoitures = {"Hummer", "Aston Martin", "Mercedes", "Toyota"};
+
+        System.out.println("Ma collection de voitures: " + collectionDeVoitures);
+
+        String collectionDeVoituresString = Arrays.toString(collectionDeVoitures);
+
+        System.out.println("Je vous présente ma véritable collection de voitures : " + collectionDeVoituresString);
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        import java.util.Arrays;
+
+        String [] collectionDeVoitures = {"Hummer", "Aston Martin", "Mercedes", "Toyota"};
+
+        System.out.println("Ma collection de voitures: " + collectionDeVoitures);
+
+        String collectionDeVoituresString = Arrays.toString(collectionDeVoitures);
+
+        System.out.println("Je vous présente ma véritable collection de voitures : " + collectionDeVoituresString);
+
+        System.out.println("Nombre de voitures: " + collectionDeVoitures.length);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        String [] collectionDeVoitures = {"Hummer", "Aston Martin", "Mercedes", "Toyota"};
+
+        System.out.println("collectionDeVoitures[0]: " + collectionDeVoitures[0]);
+
+        System.out.println("collectionDeVoitures[1]: " + collectionDeVoitures[1]);
+
+        System.out.println("collectionDeVoitures[2]: " + collectionDeVoitures[2]);
+
+        System.out.println("collectionDeVoitures[3]: " + collectionDeVoitures[3]);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        import java.util.Arrays;
+        String [] collectionDeVoitures = {"Hummer", "Aston Martin", "Mercedes", "Toyota"};
+
+        System.out.println("collectionDeVoitures[0]: " + collectionDeVoitures[0]);
+
+        System.out.println("collectionDeVoitures[1]: " + collectionDeVoitures[1]);
+
+        System.out.println("collectionDeVoitures[2]: " + collectionDeVoitures[2]);
+
+        System.out.println("collectionDeVoitures[3]: " + collectionDeVoitures[3]);
+
+        // Modification de la collection de voitures
+        collectionDeVoitures[2] = "Rolls Royce";
+        System.out.println("Voici la nouvelle voiture à l'emplacement 3: " + collectionDeVoitures[2]);
+
+        String collectionDeVoituresString = Arrays.toString(collectionDeVoitures);
+        System.out.println("Je vous présente ma véritable collection de voitures : " + collectionDeVoituresString);
+
+    }*/
 
     }
 }
